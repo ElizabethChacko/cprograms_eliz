b506b6ccef973930252548228342ce0f64aef74c
ElizabethChacko <elizabethchackovarghese@gmail.com>
2023-09-09 21:57:44 +0530

    fahrenheit to celsius

---
+++ c programs2/pattern printing.c
@@ -0,0 +1,9 @@
+/*pattern printing:       1
+                        1 2 3
+                      1 2 3 4 5
+                        1 2 3
+                          1 */
+#include<stdio.h>
+int main()
+{
+}
