4456442c62449d2ff0cdb8c0bdae39455e8b30e0
ElizabethChacko <elizabethchackovarghese@gmail.com>
2023-09-14 13:36:10 +0530

    hollow square pattern

---
+++ c programs2/hollow square pattern(star).c
@@ -0,0 +1,29 @@
+/*hollow square pattern printing: *****
+                                  *   *
+                                  *   *
+                                  *   * 
+                                  *****  */
+
+
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("enter the limit\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==1||i==n||j==1||j==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
