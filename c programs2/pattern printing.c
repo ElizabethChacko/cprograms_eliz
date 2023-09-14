895b8cebb94e183136e2e5c42f5636d88eecaa68
ElizabethChacko <129411906+ElizabethChacko@users.noreply.github.com>
2023-09-14 09:08:19 +0530

    pattern printing

--- c programs2/pattern printing.c
+++
@@ -1,9 +0,0 @@
-/*pattern printing:       1
-                        1 2 3
-                      1 2 3 4 5
-                        1 2 3
-                          1 */
-#include<stdio.h>
-int main()
-{
-}
